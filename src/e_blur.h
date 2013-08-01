0e62ee5d2e671380998a4e667da5b223a9f5b1c7
Delmo <delmosaurio@gmail.com>
2013-07-31 23:49:50 -0300

    fix GPL versrion

--- src/e_blur.h
+++ src/e_blur.h
@@ -7,7 +7,7 @@
  *      
  *      pixelate es Software Libre; Usted puede redistribuirlo y/o modificarlo 
  *      bajo los términos de la Licencia Pública General de la GNU según los 
- *      datos publicados por la Free Software Foundation, de la versión 2
+ *      datos publicados por la Free Software Foundation, de la versión 3
  *      de la Licencia,o bien (a su elección) cualquier versión posterior.
  *      
  *      Usted debe haber recibido una copia de la Licencia Pública General
